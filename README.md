e1822493c5979cc0bcce999770d802bf9c2e6b3f
Adrian Garett Sian <adriansian@gmail.com>
2025-06-30 16:34:25 +0800

    Final Submission, Lab 2: Membership Calculator

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1> Adrian Garett Sian</h1>
+<h1>Lab 2: Membership Calculator</h1>
+
+### Lab 2: Membership Calculator Output
+<img src="https://github.com/garett09/FECP-Java-Session1-ControlStructures/blob/43f36abe3fbdc670fbc24ff3057d828599b2851b/img/Screenshot%202025-06-30%20at%204.28.12%E2%80%AFPM.png"/>
+
+### Lab 2: Membership Calculator Whole Code
+<img src="https://github.com/garett09/FECP-Java-Session1-ControlStructures/blob/43f36abe3fbdc670fbc24ff3057d828599b2851b/img/Screenshot%202025-06-30%20at%204.28.38%E2%80%AFPM.png"/>
+
+### Lab 2: Membership Calculator Test Result
+<img src="https://github.com/garett09/FECP-Java-Session1-ControlStructures/blob/43f36abe3fbdc670fbc24ff3057d828599b2851b/img/Screenshot%202025-06-30%20at%204.32.20%E2%80%AFPM.png "/>
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,52 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double price = 0.00f;
+        int age = 0;
+        String membershipStatus = "";
 
+        System.out.print("Enter membership status (Regular, VIP, Premium): ");
+
+        membershipStatus = input.nextLine();
+        String convertedMembership = membershipStatus.toLowerCase();
+
+        System.out.print("Enter age: ");
+        age = input.nextInt();
+
+
+        if (convertedMembership.equals("regular")) {
+            if (age < 18) {
+                price = 50;
+            } else if (age <= 64) {
+                price = 100;
+
+            } else {
+                price = 75;
+            }
+            System.out.printf("Price: $%.2f",price);
+        } else if (convertedMembership.equals("vip")) {
+            if (age < 18) {
+                price = 75;
+            } else if (age <= 64) {
+                price = 150;
+
+            } else {
+                price = 112.5;
+            }
+            System.out.printf("Price: $%.2f",price);
+        } else if (convertedMembership.equals("premium")) {
+            if (age < 18) {
+                price = 100;
+            } else if (age <= 64) {
+                price = 200;
+
+            } else {
+                price = 150;
+            }
+            System.out.printf("Price: $%.2f",price);
+    } else {
+            System.out.print("Invalid membership status entered.");
+        }
     }
 }
